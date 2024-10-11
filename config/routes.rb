34173ad08fed305e9ceca217d19906264c0ddfc6
FannyEve PotvinLemay <fannyevepl@gmail.com>
2024-10-10 20:01:59 -0400

    mise à jour

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  # Route pour afficher le formulaire de création d'une nouvelle tâche
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # Route pour lister les tâches
+  get 'tasks', to: 'tasks#index'
+
+  # Route pour afficher les détails d'une tâche
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Route pour soumettre le formulaire et créer une nouvelle tâche
+  post 'tasks', to: 'tasks#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
